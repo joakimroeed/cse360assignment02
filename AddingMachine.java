0ebfe5c749eefd544298a38f4d8f4b5ab1303ddc
Joakim Roeed <43100776+joakimroeed@users.noreply.github.com>
2020-10-07 18:03:28 -0700

    Update AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -7,20 +7,20 @@ public class AddingMachine {
 	    total = 0;  // not needed - included for clarity
 	  }
 	  
-	  public int getTotal () {
+	  public int getTotal () { // Get the total value
 	    return 0;
 	  }
 	  
-	  public void add (int value) {
+	  public void add (int value) { // Adding value to the total
 	  }
 
-	  public void subtract (int value) {
+	  public void subtract (int value) { // Subtracting value to the total
 	  }
 
-	  public String toString () {
+	  public String toString () { 
 	    return "";
 	  }
 
-	  public void clear() {
+	  public void clear() { // Resetting total to 0
 	  }
 }
