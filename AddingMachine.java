c98b750bc6584b84ebd9e0cd8860b20a216658a9
Joakim Roeed <43100776+joakimroeed@users.noreply.github.com>
2020-10-07 18:43:25 -0700

    Update AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,26 +1,45 @@
 package cse360assignment02;
 
-public class AddingMachine {
+//https://github.com/joakimroeed/cse360assignment02
+//Joakim Roeed
+//1215162468
+
+
+public class AddingMachine {  
+	  //public static void main(String[] args) {
+	  //	  AddingMachine comp = new AddingMachine();
+	  //	  comp.add(4);
+	  //	  comp.subtract(2);
+	  //	  comp.add(5);
+	  //	  System.out.println(comp.toString());
+	  //}
+	
 	  private int total;
+	  private String string = "0";
 	  
 	  public AddingMachine () {
-	    total = 0;  // not needed - included for clarity
+		  total = 0;  // not needed - included for clarity
 	  }
 	  
 	  public int getTotal () { // Get the total value
-	    return 0;
+		  return total; 
 	  }
 	  
-	  public void add (int value) { // Adding value to the total
+	  public void add (int value) { // Add value to total
+		  total = total + value;
+		  string += " + " + value;
 	  }
 
-	  public void subtract (int value) { // Subtracting value to the total
+	  public void subtract (int value) { // Subtract value from total
+		  total = total - value;
+		  string += " - " + value;
 	  }
 
-	  public String toString () { 
-	    return "";
+	  public String toString () {
+		  return string;
 	  }
 
-	  public void clear() { // Resetting total to 0
+	  public void clear() { // Reset the total to 0
+		  total = 0;
 	  }
 }
